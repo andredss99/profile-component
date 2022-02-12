c6385a428e7a2411a16be43f01dc81a00f4fac72
Andre Sousa <andre.sousa40@etec.sp.gov.br>
2022-02-12 18:39:13 -0300

    IT'S ALIVE! consumindo API do Github com vanilla JS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const baseUrl = 'https://api.github.com/users/andredss99';
+const starsUrl = 'https://api.github.com/users/andredss99/starred';
+
+const requestProfileInfo = fetch(baseUrl)
+.then(response => response.json())
+.then(jsonBody => {
+    document.querySelector('.profile-image img').src = jsonBody.avatar_url;
+    document.querySelector('.profile-info h1').innerHTML = jsonBody.name;
+    document.querySelector('.profile-info h4').innerHTML = jsonBody.login;
+    document.querySelector('.projects h1').innerHTML = jsonBody.public_repos;
+    document.querySelector('.followers h1').innerHTML = jsonBody.followers;
+});
+
+const requestStarsInfo = fetch(starsUrl)
+.then(response => response.json())
+.then(jsonBody => {
+    document.querySelector('.stars h1').innerHTML = jsonBody.length;
+});
